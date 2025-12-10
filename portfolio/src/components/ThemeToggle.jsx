909d0b8f323c581acb60e67264594eb4e3b2a2bc
MohammadHSA <MohammadHSafar@hotmail.com>
2025-12-09 21:59:38 -0700

    I finished the groundwork

---
+++ portfolio/src/components/ThemeToggle.jsx
@@ -0,0 +1,15 @@
+import { Sun, Moon} from "lucide-react";
+import { useState } from "react";
+
+export const ThemeToggle = () => {
+    const[isDarkMode, setIsDarkMode] = useState(false);
+    return ( 
+    
+    <button>
+        {isDarkMode ? <Sun /> : <Moon />}
+    </button>
+
+
+
+);
+}
---
+++ portfolio/src/pages/home.jsx
@@ -0,0 +1,16 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/*theem toggle*/}
+      <ThemeToggle />
+
+      {/*background effects*/}
+
+      {/*navigation bar*/}
+
+      {/*footer*/}
+    </div>
+  );
+};
