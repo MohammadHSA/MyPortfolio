057a942f91092043717013189b673f4f989d4f23
Mohammad Safar <mohammadhsafar@hotmail.com>
2026-03-29 02:39:59 -0700

    fix: resolve dependencies and pathing for final deploy

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -7,7 +7,6 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/MyPortfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -15,3 +14,4 @@ export default defineConfig({
   },
 
 })
+
