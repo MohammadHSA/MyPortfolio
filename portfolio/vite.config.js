d6ef557e4d77dbb8f615b77cd650324b21dbbb03
Mohammad Safar <mohammadhsafar@hotmail.com>
2026-03-29 03:10:49 -0700

    Bug fix 1

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,14 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/MyPortfolio/', // This must match your GitHub Repo name exactly
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-
 })
 
